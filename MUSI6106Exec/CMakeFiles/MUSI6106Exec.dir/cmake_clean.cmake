e316d82867c941935c3aed952d4606108080c11f
sandeep <sandeep.cr007@gmail.com>
2020-01-13 15:04:56 -0500

    initial try

---
+++ MUSI6106Exec/CMakeFiles/MUSI6106Exec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/MUSI6106Exec"
+  "../bin/MUSI6106Exec.pdb"
+  "CMakeFiles/MUSI6106Exec.dir/MUSI6106Exec.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MUSI6106Exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -1,9 +1,9 @@
 
 #include <iostream>
+#include <fstream>
 #include <ctime>
 
 #include "MUSI6106Config.h"
-
 #include "AudioFileIf.h"
 
 using std::cout;
@@ -28,27 +28,41 @@ int main(int argc, char* argv[])
     CAudioFileIf            *phAudioFile = 0;
     std::fstream            hOutputFile;
     CAudioFileIf::FileSpec_t stFileSpec;
-
+    
     showClInfo();
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
- 
+    sInputFilePath = argv[1];
+    sOutputFilePath = argv[2];
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
- 
+    
+    stFileSpec.eFormat = CAudioFileIf::FileFormat_t::kFileFormatWav;
+    stFileSpec.eBitStreamType = CAudioFileIf::BitStream_t::kFileBitStreamInt16;
+    stFileSpec.iNumChannels = 2;
+    stFileSpec.fSampleRateInHz = 44100.0;
+    
+    CAudioFileIf::create(phAudioFile);
+    
+    phAudioFile->openFile(sInputFilePath,CAudioFileIf::FileIoType_t::kFileRead , &stFileSpec);
     //////////////////////////////////////////////////////////////////////////////
     // open the output text file
- 
+    cout<< sOutputFilePath <<endl;
+    hOutputFile.open(sOutputFilePath, std::fstream::out);
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
- 
+    long long lenSec;
+    phAudioFile->getLength(lenSec);
+    
     //////////////////////////////////////////////////////////////////////////////
     // get audio data and write it to the output text file (one column per channel)
-
+    
+    
     //////////////////////////////////////////////////////////////////////////////
     // clean-up (close files and free memory)
-
+    CAudioFileIf::destroy(phAudioFile);
+    hOutputFile.close();
     // all done
     return 0;
 
