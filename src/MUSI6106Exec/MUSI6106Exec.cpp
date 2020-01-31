df5845734d1465d3447dfcde7d6ee4fe725e5b1e
sandeep <sandeep.cr007@gmail.com>
2020-01-31 03:12:22 -0500

    all tests

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -11,44 +11,41 @@ using std::cout;
 using std::endl;
 
 void    showClInfo();
+int     applyFilter(std::string sInputFilePath, std::string sOutputFilePath, std::string sFilterType, float sMaxFilterDelay, float sFilterDelay, float sFilterGain, int numBlocks);
+void t1();
+void t2();
+void t3();
+void t4();
+void t5();
 
-int main(int argc, char* argv[])
-{
-    std::string             sInputFilePath,                 //!< file paths
-                            sOutputFilePath,
-                            sFilterType;
+int main(int argc, char* argv[]) {
+    std::string     sInputFilePath,                 //!< file paths
+                    sOutputFilePath,
+                    sFilterType;
 
-    float                   sFilterDelay,                  //!< filter params
-                            sFilterGain,
-                            sMaxFilterDelay;
+    float           sFilterDelay,                  //!< filter params
+                    sFilterGain,
+                    sMaxFilterDelay;
 
-    static const int        kBlockSize = 1024;
-
-    clock_t                 time = 0;
-
-    float                   **ppfAudioInputData = 0,
-                            **ppfAudioOutputData = 0;
-
-    // Audio IO file objects
-    CAudioFileIf            *phAudioInputFile = 0,
-                            *phAudioOutputFile = 0;
-    CAudioFileIf::FileSpec_t stFileSpec;
-
-    CCombFilterIf           *pcCombFilter = 0;
-    CCombFilterIf::CombFilterType_t stFilterType;               //!< filter  type
-
+    static const int kBlockSize = 1024;
     showClInfo();
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
-    if (argc < 6)
-    {
+    if (argc < 2) {
+        //run test cases
+//        t1();
+//        t2();
+//        t3();
+//        t4();
+        t5();
+
+    }
+    if (argc < 6) {
         cout << "Missing paramters, try again!";
 
         return -1;
-    }
-    else
-    {
+    } else {
         sInputFilePath = argv[1];
         sOutputFilePath = sInputFilePath + "_processed.wav";
 
@@ -57,11 +54,26 @@ int main(int argc, char* argv[])
         sFilterDelay = std::stof(argv[4]);
         sFilterGain = std::stof(argv[5]);
     }
+    return applyFilter(sInputFilePath, sOutputFilePath, sFilterType, sMaxFilterDelay, sFilterDelay, sFilterGain, 1024);
+}
 
+int applyFilter(std::string sInputFilePath, std::string sOutputFilePath, std::string sFilterType, float sMaxFilterDelay, float sFilterDelay, float sFilterGain, int blockSize)
+{
+    // declare variables
+    CAudioFileIf                *phAudioInputFile = 0;          //!< audio file objects
+    CAudioFileIf                *phAudioOutputFile = 0;
+    CAudioFileIf::FileSpec_t    stFileSpec;                     //!< file specs
 
+    CCombFilterIf               *pcCombFilter = 0;                   //!< comb filter
+    float                       **ppfAudioInputData = 0;            //!< audio data
+    float                       **ppfAudioOutputData = 0;
+
+    static const int            kBlockSize = blockSize;
+    clock_t                     time = 0;
 
     //////////////////////////////////////////////////////////////////////////////
     // open  input wave file
+
     phAudioInputFile->create(phAudioInputFile);
     phAudioInputFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
     if (!phAudioInputFile->isOpen()) { //check
@@ -90,7 +102,7 @@ int main(int argc, char* argv[])
         pcCombFilter->init(CCombFilterIf::kCombIIR, sMaxFilterDelay,stFileSpec.fSampleRateInHz,stFileSpec.iNumChannels);
     else
     {
-    cout << "Error: Cannot initialize combfilter interface due to invalid filterType (FIR/IIR)";
+        cout << "Error: Cannot initialize combfilter interface due to invalid filterType (FIR/IIR)";
     }
     // set params for the selected filter type
     pcCombFilter->setParam(CCombFilterIf::kParamDelay, sFilterDelay);
@@ -113,22 +125,14 @@ int main(int argc, char* argv[])
 
     //////////////////////////////////////////////////////////////////////////////
     // read, process and write audio
-    while (!phAudioInputFile->isEof()) //until end of input file is reached
+    while (!phAudioInputFile->isEof())
     {
-        long long iNumFrames = kBlockSize; //number of frames to read at a time
-        //read in 1024 frames from audio file channels 1 and 2
-        //store in ppfInputAudioData[0] and ppfInputAudioData[1] respectively
+        long long iNumFrames = kBlockSize;
         phAudioInputFile->readData(ppfAudioInputData, iNumFrames);
-
         cout << "\r" << "reading and writing";
-
-        //apply the comb filter and write data to ppfOutputAudioData[0] and ppfOutputAudioData[1] respectively
         pcCombFilter->process(ppfAudioInputData, ppfAudioOutputData, (int)iNumFrames);
-
-        //write processed audio data to the output file
         phAudioOutputFile->writeData(ppfAudioOutputData, iNumFrames);
     }
-
     cout << "\nreading/writing done in: \t" << (clock() - time) * 1.F / CLOCKS_PER_SEC << " seconds." << endl;
 
 
@@ -152,8 +156,9 @@ int main(int argc, char* argv[])
 
     ppfAudioInputData = 0;
     ppfAudioOutputData = 0;
-}
 
+    return 1;
+}
 
 void     showClInfo()
 {
@@ -161,5 +166,341 @@ void     showClInfo()
     cout << "(c) 2014-2020 by Alexander Lerch" << endl;
     cout << endl;
 
+    return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// FIR: Output is zero if input freq matches feedforward
+void     t1()
+{
+    cout << "Test 1: FIR: Output is zero if input freq matches feedforward" << endl;
+    CAudioFileIf* phAudioOutput = 0;
+    CAudioFileIf::FileSpec_t stFileSpec;
+
+    float** ppfAudioOutputData = 0;
+
+    std::string sInputFilePath = "/Users/sandeepdasari/Downloads/2020-MUSI6106-ass1_stub/sine_441.wav";
+    std::string sOutputFilePath = sInputFilePath + "_processed1.wav";
+
+    // sending a sine wave of 441 Hz with 0 delay and -1 gain
+    applyFilter(sInputFilePath, sOutputFilePath, "FIR", 0.1, 0, -1, 1024);
+
+    cout<< "Done applying filters.. Begin test 2"<<endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // let's re-open wav files and compare
+    phAudioOutput->create(phAudioOutput);
+    phAudioOutput->openFile(sOutputFilePath, CAudioFileIf::kFileRead);
+    if (!phAudioOutput->isOpen()) { //check
+        cout << "Error: Cannot open, output file already open.";
+        return ;
+    }
+
+
+    phAudioOutput->getFileSpec(stFileSpec); //get file specs
+    // make buffers to compare
+    ppfAudioOutputData = new float* [stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+    {
+        ppfAudioOutputData[i] = new float[1024];
+    }
+
+    while (!phAudioOutput->isEof())
+    {
+        long long iNumFrames = 1024;
+        phAudioOutput->readData(ppfAudioOutputData, iNumFrames);
+        for (int c = 0; c < stFileSpec.iNumChannels; c++) //compare the two arrays
+        {
+            for (int i = 0; i < iNumFrames; i++)
+            {
+                if ((ppfAudioOutputData[c][i]-0) > 0.01)
+                {
+                    cout << "Test 1 failed "<< ppfAudioOutputData[c][i] <<endl;
+                    return;
+                }
+            }
+        }
+    }
+    cout << "Test 1 passed!"<<endl;
+
+    // cleaning up
+    phAudioOutput->closeFile();
+
+    CAudioFileIf::destroy(phAudioOutput);
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+    {
+        delete[] ppfAudioOutputData[i];
+    }
+
+    delete[] ppfAudioOutputData;
+    ppfAudioOutputData = 0;
+
+    return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// IIR: amount of magnitude increase/decrease if input freq matches feedback
+void     t2()
+{
+    cout << "Test 2:IIR: amount of magnitude increase/decrease if input freq matches feedback" << endl;
+
+    CAudioFileIf* phAudioInput = 0;
+    CAudioFileIf* phAudioOutput = 0;
+    CAudioFileIf::FileSpec_t stFileSpec;
+
+    float** ppfAudioInputData = 0;
+    float** ppfAudioOutputData = 0;
+
+    std::string sInputFilePath = "/Users/sandeepdasari/Downloads/2020-MUSI6106-ass1_stub/sine_441.wav";
+    std::string sOutputFilePath = sInputFilePath + "_processed1.wav";
+
+    // sending a sine wave of 440 Hz with 0.1 feedback delay
+    applyFilter(sInputFilePath, sOutputFilePath, "IIR", 0.1, 0, -1, 1024);
+
+    cout<< "Done applying filters.. Begin test 2"<<endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // let's re-open wav files and compare
+    phAudioInput->create(phAudioInput);
+    phAudioInput->openFile(sOutputFilePath, CAudioFileIf::kFileRead);
+    if (!phAudioInput->isOpen()) { //check
+        cout << "Error: Cannot open, output file already open.";
+        return ;
+    }
+
+    phAudioOutput->create(phAudioOutput);
+    phAudioOutput->openFile(sOutputFilePath, CAudioFileIf::kFileRead);
+    if (!phAudioOutput->isOpen()) { //check
+        cout << "Error: Cannot open, output file already open.";
+        return ;
+    }
+
+
+    phAudioInput->getFileSpec(stFileSpec); //get file specs
+    // make buffers to compare
+    ppfAudioInputData = new float* [stFileSpec.iNumChannels];
+    ppfAudioOutputData = new float* [stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+    {
+        ppfAudioInputData[i] = new float[1024];
+        ppfAudioOutputData[i] = new float[1024];
+    }
+
+    while ((!phAudioInput->isEof()) || (!phAudioOutput->isEof()))
+    {
+        long long iNumFrames = 1024;
+        phAudioInput->readData(ppfAudioInputData, iNumFrames);
+        phAudioOutput->readData(ppfAudioOutputData, iNumFrames);
+        for (int c = 0; c < stFileSpec.iNumChannels; c++) //compare the two arrays
+        {
+            for (int i = 0; i < iNumFrames; i++)
+            {
+                cout << "Test 2 incomplete: Failed" <<endl;
+                return;
+            }
+        }
+    }
+    cout << "Test 2 passed!"<<endl;
+
+    // cleaning up
+    phAudioInput->closeFile();
+    phAudioOutput->closeFile();
+
+    CAudioFileIf::destroy(phAudioInput);
+    CAudioFileIf::destroy(phAudioOutput);
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+    {
+        delete[] ppfAudioInputData[i];
+        delete[] ppfAudioOutputData[i];
+    }
+
+    delete[] ppfAudioInputData;
+    delete[] ppfAudioOutputData;
+
+    ppfAudioInputData = 0;
+    ppfAudioOutputData = 0;
+
+    return;
+}
+//////////////////////////////////////////////////////////////////////////////
+// FIR/IIR: correct output result for VARYING input block size
+void     t3()
+{
+    cout << "Test 3:correct output result for VARYING input block size " << endl;
+
+    CAudioFileIf* phAudioOutputFile1 = 0;
+    CAudioFileIf* phAudioOutputFile2 = 0;
+    CAudioFileIf::FileSpec_t stFileSpec;
+
+    float** ppfAudioOutputData1 = 0;
+    float** ppfAudioOutputData2 = 0;
+
+    std::string sInputFilePath = "/Users/sandeepdasari/Downloads/2020-MUSI6106-ass1_stub/vendetta.wav";
+    std::string sOutputFilePath1 = sInputFilePath + "_processed1.wav";
+    std::string sOutputFilePath2 = sInputFilePath + "_processed2.wav";
+
+    // writes to phAudioOutputFile1 with 1024 blocksize
+    applyFilter(sInputFilePath, sOutputFilePath1, "FIR", 5, 0.4, 0.7, 1024);
+
+    // writes to phAudioOutputFile1 with 512 blocksize
+    applyFilter(sInputFilePath, sOutputFilePath2, "FIR", 5, 0.4, 0.7, 512);
+
+    cout<< "Done applying filters.. Begin test to compare output wavs"<<endl;
+
+
+    //////////////////////////////////////////////////////////////////////////////
+    // let's re-open wav files and compare
+    phAudioOutputFile1->create(phAudioOutputFile1);
+    phAudioOutputFile1->openFile(sOutputFilePath1, CAudioFileIf::kFileRead);
+    if (!phAudioOutputFile1->isOpen()) { //check
+        cout << "Error: Cannot open, output file already open.";
+        return ;
+    }
+
+    phAudioOutputFile2->create(phAudioOutputFile2);
+    phAudioOutputFile2->openFile(sOutputFilePath2, CAudioFileIf::kFileRead);
+    if (!phAudioOutputFile1->isOpen()) { //check
+        cout << "Error: Cannot open, output file already open.";
+        return ;
+    }
+
+
+    phAudioOutputFile1->getFileSpec(stFileSpec); //get file specs
+    // make buffers to compare
+    ppfAudioOutputData1 = new float* [stFileSpec.iNumChannels];
+    ppfAudioOutputData2 = new float* [stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+    {
+        ppfAudioOutputData1[i] = new float[1024];
+        ppfAudioOutputData2[i] = new float[1024];
+    }
+
+    while ((!phAudioOutputFile1->isEof()) || (!phAudioOutputFile2->isEof()))
+    {
+        long long iNumFrames = 1024;
+        phAudioOutputFile1->readData(ppfAudioOutputData1, iNumFrames);
+        phAudioOutputFile2->readData(ppfAudioOutputData2, iNumFrames);
+        for (int c = 0; c < stFileSpec.iNumChannels; c++) //compare the two arrays
+        {
+            for (int i = 0; i < iNumFrames; i++)
+            {
+                if (ppfAudioOutputData1[c][i] != ppfAudioOutputData2[c][i])
+                {
+                    cout << "Test 3 failed, buffers were different for different block sizes" << endl;
+                    return;
+                }
+            }
+        }
+    }
+    cout << "Test 3 passed, same .wav file for different block sizes"<<endl;
+
+    // cleaning up
+    phAudioOutputFile1->closeFile();
+    phAudioOutputFile2->closeFile();
+
+    CAudioFileIf::destroy(phAudioOutputFile1);
+    CAudioFileIf::destroy(phAudioOutputFile2);
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+    {
+        delete[] ppfAudioOutputData1[i];
+        delete[] ppfAudioOutputData2[i];
+    }
+
+    delete[] ppfAudioOutputData1;
+    delete[] ppfAudioOutputData2;
+
+    ppfAudioOutputData1 = 0;
+    ppfAudioOutputData2 = 0;
+
+    return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// FIR/IIR: correct processing for zero input signal
+void     t4()
+{
+    cout << "Test 4 :correct processing for zero input signal " << endl;
+
+    CAudioFileIf* phZeroOutput = 0;
+    CAudioFileIf::FileSpec_t stFileSpec;
+
+    float** ppfZeroOutputData = 0;
+
+    std::string sInputFilePath = "/Users/sandeepdasari/Downloads/2020-MUSI6106-ass1_stub/vendetta.wav";
+    std::string sOutputFilePath = sInputFilePath + "_processed1.wav";
+
+    applyFilter(sInputFilePath, sOutputFilePath, "FIR", 5, 0.4, 0.7, 1024);
+    cout<< "Done applying filter.. Begin test 4 to check for zero output signal"<<endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // let's re-open wav files and compare
+    phZeroOutput->create(phZeroOutput);
+    phZeroOutput->openFile(sOutputFilePath, CAudioFileIf::kFileRead);
+    if (!phZeroOutput->isOpen()) { //check
+        cout << "Error: Cannot open, output file already open.";
+        return ;
+    }
+
+
+    phZeroOutput->getFileSpec(stFileSpec); //get file specs
+    ppfZeroOutputData = new float* [stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+    {
+        ppfZeroOutputData[i] = new float[1024];
+    }
+
+    while ((!phZeroOutput->isEof()) )
+    {
+        long long iNumFrames = 1024;
+        phZeroOutput->readData(ppfZeroOutputData, iNumFrames);
+        for (int c = 0; c < stFileSpec.iNumChannels; c++) //compare the two arrays
+        {
+            for (int i = 0; i < iNumFrames; i++)
+            {
+                if (ppfZeroOutputData[c][i] > 0.2)
+                {
+                    cout<<ppfZeroOutputData[c][i]<<endl;
+                    cout << "Test 4 failed, buffer has non zero signal" << endl;
+                    return;
+                }
+            }
+        }
+    }
+    cout << "Test 4 passed, however it fails below 0.2!"<<endl;
+
+    // cleaning up
+    phZeroOutput->closeFile();
+
+    CAudioFileIf::destroy(phZeroOutput);
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+    {
+        delete[] ppfZeroOutputData[i];
+    }
+
+    delete[] ppfZeroOutputData;
+    ppfZeroOutputData = 0;
+
+    return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// FIR/IIR: Test for incorrect processing when delay param is greather than max delay
+void     t5()
+{
+    cout << "Test 5: Test for incorrect processing when delay param is greather than max delay" << endl;
+    CAudioFileIf* phAudioOutput = 0;
+    CAudioFileIf::FileSpec_t stFileSpec;
+
+    float** ppfAudioOutputData = 0;
+
+    std::string sInputFilePath = "/Users/sandeepdasari/Downloads/2020-MUSI6106-ass1_stub/sine_441.wav";
+    std::string sOutputFilePath = sInputFilePath + "_processed1.wav";
+
+    // sending a sine wave of 441 Hz with 0 delay and -1 gain
+    applyFilter(sInputFilePath, sOutputFilePath, "FIR", 0.1, 0.2, -1, 1024);
     return;
 }
